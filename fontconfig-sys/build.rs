61a48343ceacc39f994b9c9b114450a0957573f4
VernoxVernax <vernoxvernax@gmail.com>
2023-09-15 17:30:52 +0200

    easier building on windows

--- fontconfig-sys/build.rs
+++ fontconfig-sys/build.rs
@@ -1,10 +1,15 @@
 fn main() {
-    println!("cargo:rerun-if-env-changed=RUST_FONTCONFIG_DLOPEN");
     let dlopen = std::env::var_os("RUST_FONTCONFIG_DLOPEN").is_some();
-    if dlopen {
-        println!("cargo:rustc-cfg=feature=\"dlopen\"");
-    }
-    if !(dlopen || cfg!(feature = "dlopen")) {
-        pkg_config::find_library("fontconfig").unwrap();
+    println!("cargo:rerun-if-env-changed=RUST_FONTCONFIG_DLOPEN");
+    if !cfg!(windows) || dlopen {
+        if dlopen {
+            println!("cargo:rustc-cfg=feature=\"dlopen\"");
+        }
+        if !(dlopen || cfg!(feature = "dlopen")) {
+            pkg_config::find_library("fontconfig").unwrap();
+        }
+    } else if cfg!(windows) {
+        println!("cargo:rustc-link-lib=fontconfig");
+        println!("cargo:rustc-link-lib=freetype");
     }
 }
